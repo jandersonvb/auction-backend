65dfee3798bc2f7152eca6e4c2ee413b6c70132a
jandersonvb <jandersonvb.dev@gmail.com>
2024-05-14 18:52:23 -0300

    feat: create resource users

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from './database/database.module';
 import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    DatabaseModule],
+    DatabaseModule,
+    UsersModule],
   controllers: [],
   providers: [],
 })
