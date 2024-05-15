1cc8a2581e24a4c43ab9da7fbad35189e7a478ed
jandersonvb <jandersonvb.dev@gmail.com>
2024-05-15 13:56:31 -0300

    feat: create resource auctions

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,13 +4,15 @@ import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
 import { BooksModule } from './books/books.module';
 import { OffersModule } from './offers/offers.module';
+import { AuctionsModule } from './auctions/auctions.module';
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     DatabaseModule,
     UsersModule,
     BooksModule,
-    OffersModule],
+    OffersModule,
+    AuctionsModule],
   controllers: [],
   providers: [],
 })
