8e492c3f67cf77b5de33822dff2165e47a6de406
jandersonvb <jandersonvb.dev@gmail.com>
2024-05-15 13:55:56 -0300

    feat: create resource offers

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,14 @@ import { DatabaseModule } from './database/database.module';
 import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
 import { BooksModule } from './books/books.module';
+import { OffersModule } from './offers/offers.module';
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     DatabaseModule,
     UsersModule,
-    BooksModule],
+    BooksModule,
+    OffersModule],
   controllers: [],
   providers: [],
 })
